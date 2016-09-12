8cb2d92bb1dc3153ce689d0328eecd3f33d0e70b
chachaxw <867571123@qq.com>
2016-09-12 23:47:59 +0800

    add images

--- js/main.js
+++ js/main.js
@@ -1,12 +1,77 @@
 ;(function(){
     var man_all = [
-        '刘克亮','吴亮亮','江育麟',
-        '张彦蔚','毛阿斌','黄哲宇',
-        '黄志坡','陈少芳','柯宇楠',
-        '汪叶金','王潇','罗云志',
-        '陈式坚','陈宇哲','周伟'
+        {
+            name: '周伟',
+            avatar: 'images/zhouwei.png',
+        }, {
+            name: '程少芳',
+            avatar: 'images/chengshaofang.png',
+        }, {
+            name: '陈宇哲',
+            avatar: 'images/chenyuzhe.png',
+        }, {
+            name: '江育麟',
+            avatar: 'images/jiangyulin.png',
+        }, {
+            name: '柯宇楠',
+            avatar: 'images/keyunan.png',
+        }, {
+            name: '张彦蔚',
+            avatar: 'images/zhangyanwei.png',
+        }, {
+            name: '李昌智',
+            avatar: 'images/lichangzhi.png',
+        }, {
+            name: '李汉健',
+            avatar: 'images/lihanjian.png',
+        }, {
+            name: '刘克亮',
+            avatar: 'images/liukeliang.png',
+        }, {
+            name: '罗云志',
+            avatar: 'images/luoyunzhi.png',
+        }, {
+            name: '毛阿斌',
+            avatar: 'images/maoabing.png',
+        }, {
+            name: '王斌',
+            avatar: 'images/wangbin.png',
+        }, {
+            name: '吴亮亮',
+            avatar: 'images/wuliangliang.png',
+        }, {
+            name: '李汉健',
+            avatar: 'images/lihanjian.png',
+        }, {
+            name: '汪金叶',
+            avatar: 'images/wangjinye.png',
+        }, {
+            name: '王潇',
+            avatar: 'images/wangxiao.png',
+        },
     ];
-    var women_all = ['林素娜','张嘉炜','杨洁','李小霞','叶颖诗'];
+    var women_all = [
+        {
+            name: '杨洁',
+            avatar: 'images/yangjie.png',
+        }, {
+            name: '林素娜',
+            avatar: 'images/linsuna.png',
+        }, {
+            name: '张嘉炜',
+            avatar: 'images/zhangjiawei.png',
+        }, {
+            name: '谢昕怡',
+            avatar: 'images/xiexinyi.png',
+        }, {
+            name: '叶颖诗',
+            avatar: 'images/yeyinshi.png',
+        }, {
+            name: '彭敬嫦',
+            avatar: 'images/pengjingchang.png',
+        }
+    ];
+
     var all = man_all.concat(women_all);
     var group_num = 4;
 
@@ -25,13 +90,14 @@
             for(var i = 0, l = j.length; i < l ; i ++) {
                 flex.eq(i).empty();
                 var t1 = setTimeout((function(i) {
+                    console.log(i);
                     return function() {
                         for(var k = 0, h = j[i].length; k < h; k++) {
                             var t2 = setTimeout((function(k){
                                 return function() {
-                                    name = j[i][k];
-                                    var span = '<div><span class="round animated bounceInDown">' + name + '</span></div>';
-
+                                    name = j[i][k].name;
+                                    avatar = j[i][k].avatar;
+                                    var span = '<div><span class="round animated bounceInDown" style="background-image: url(' + avatar + ')"><span class="over-layout"></span>' + name + '</span></div>';
                                     flex.eq(i).append(span);
                                     if($('.group-list .round').length == all.length){
                                         $('.group-list').attr('data-loading', 'false');
@@ -69,5 +135,6 @@ function s(man_all, women_all, group_num, all) {
     for (var i = 0; i < all.length; i++) {
        rs[i % group_num].push(all[i]);
     }
+    console.log(rs);
     return rs;
 }
